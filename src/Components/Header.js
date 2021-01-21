692e534decb14e30737a2be3579472e24b31e37e
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-21 15:45:17 +0530

    button turned to clickable icon

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './Header.css';
 import PersonIcon from "@material-ui/icons/Person";
+import IconButton from "@material-ui/core/IconButton";
 
 function Header() {
     return (
         <div className="header">
 
             <h2>Header component</h2>
-            <PersonIcon fontSize="Large" className="header_icon" />
+            <IconButton>
+              <PersonIcon fontSize="Large" className="header_icon" />
+            </IconButton>
 
         </div>
     )
