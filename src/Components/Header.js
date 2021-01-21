38dc9045e529d9129d432dd7d06511588abaa7ab
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-21 15:36:13 +0530

    inserted icon

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Header.css';
+import PersonIcon from "@material-ui/icons/Person";
 
 function Header() {
     return (
         <div className="header">
-            
+
             <h2>Header component</h2>
+            <PersonIcon />
 
         </div>
     )
