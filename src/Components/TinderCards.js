d2d3af4fbc62cdd8b74def6c5ab34acb0310c903
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-21 18:15:25 +0530

    created state

--- src/Components/TinderCards.js
+++ src/Components/TinderCards.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./TinderCards.css";
 
 function TinderCards() {
+  const [people, setPeople] = useState([]);
 
   return (
     <div className="tinderCards">  
