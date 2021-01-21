384604aeef6e88f01d9b566b9f650c85b1d4788e
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-21 14:47:36 +0530

    imported 'Header' component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Header from './Components/Header';
 
 function App() {
   return (
@@ -7,9 +8,11 @@ function App() {
       <h1> MERN TINDER clone </h1>    
 
       {/* Header */}
+      <Header />
+
       {/* TinderCards */}
       {/* SwipeButtons */}
-        
+
     </div>
   );
 }
