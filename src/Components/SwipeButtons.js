bfeef9f7df41a239f9a4a702c0eec8577a2534d5
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-22 11:05:06 +0530

    created component

---
+++ src/Components/SwipeButtons.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function SwipeButtons() {
+  return (
+    <div className="swipeButtons">   
+
+      SwipeButtons
+
+    </div>
+  );
+}
+
+export default SwipeButtons;
