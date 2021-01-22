71c1fb88a5e910518952ec1f835bbfccfb3895e8
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-22 15:15:55 +0530

    created test api

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,15 @@ import express from 'express'
 import mongoose from 'mongoose'
 
 // App config
+const app = express(); // created an instance
+const port = process.env.PORT || 8001
 
 // Middlewares
 
 // DB config
 
 // API Endpoints
+app.get('/', (req, res) => res.status(200).send("Hello Pragrammers!!!"));
 
 // Listener
+app.listen(port, () => console.log(`Listening on localhost: ${port}`))
