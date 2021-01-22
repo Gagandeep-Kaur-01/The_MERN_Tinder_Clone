10ef980cb1161d32412ddbc7170e8b59af24b327
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-22 15:44:32 +0530

    connecting database to mongo db

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ import mongoose from 'mongoose'
 
 // App config
 const app = express(); // created an instance
-const port = process.env.PORT || 8001
+const port = process.env.PORT || 8001;
+const connection_url = 'mongodb+srv://admin:<password>@cluster0.hrsea.mongodb.net/<dbname>?retryWrites=true&w=majority'
 
 // Middlewares
 
