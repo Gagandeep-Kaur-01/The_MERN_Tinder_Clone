de5b771c8d1c749c5af109f960d8dba8f3e620b6
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-22 15:49:03 +0530

    connecting database to mongo db

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,16 @@ import mongoose from 'mongoose'
 // App config
 const app = express(); // created an instance
 const port = process.env.PORT || 8001;
-const connection_url = 'mongodb+srv://admin:<password>@cluster0.hrsea.mongodb.net/<dbname>?retryWrites=true&w=majority'
+const connection_url = `mongodb+srv://admin:*********@cluster0.hrsea.mongodb.net/tinderdb?retryWrites=true&w=majority`
 
 // Middlewares
 
 // DB config
+mongoose.connect(connection_url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+})
 
 // API Endpoints
 app.get('/', (req, res) => res.status(200).send("Hello Pragrammers!!!"));
