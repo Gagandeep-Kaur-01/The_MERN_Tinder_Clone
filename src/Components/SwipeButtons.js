d6997bd23bbc80c73c335f453a7c29b9cddabc36
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-22 12:10:09 +0530

    inserted the buttons

--- src/Components/SwipeButtons.js
+++ src/Components/SwipeButtons.js
@@ -10,9 +10,27 @@ import './SwipeButtons.css'
 
 function SwipeButtons() {
   return (
-    <div className="swipeButtons">   
+    <div classNamse="swipeButtons">   
 
-      SwipeButtons
+        <IconButton className="swipeButtons__repeat">
+          <ReplayIcon fontSize="large" />
+        </IconButton>
+
+        <IconButton className="swipeButtons__left">
+          <CloseIcon fontSize="large" />
+        </IconButton>
+
+        <IconButton className="swipeButtons__start">
+          <StarRateIcon fontSize="large" />
+        </IconButton>
+
+        <IconButton className="swipeButtons__right">
+          <FavoriteIcon fontSize="large" />
+        </IconButton>
+
+        <IconButton className="swipeButtons__lightning">
+          <FlashOnIcon fontSize="large" />
+        </IconButton>
 
     </div>
   );
