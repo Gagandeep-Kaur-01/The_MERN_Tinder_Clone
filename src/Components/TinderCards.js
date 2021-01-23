045f9e439388297ed3ac1e8149f41e9dd7fc3f68
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-23 10:13:54 +0530

    imported

--- src/Components/TinderCards.js
+++ src/Components/TinderCards.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import { SwipeableDrawer } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 import TinderCard from "react-tinder-card";
